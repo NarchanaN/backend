9cde4d287c963fe9173360b993b315edc5509bdc
NarchanaN <archanasingh160197@gmail.com>
2022-04-02 18:15:06 +0530

    k

--- model/admin_model.js
+++ model/admin_model.js
@@ -13,7 +13,7 @@ const adminSchema = new mongoose.Schema({
     },
     password:{
         type:String,
-        required:true,
+        required:true
     }
 })
 module.exports = mongoose.model('admin',adminSchema); 
